05e1cf03b65d1e9280d861ae0116b9a20d8f09f0
jayeshuk <jayeshukalkar@gmail.com>
2020-07-31 21:21:19 +0530

    Dashboards Added

---
+++ front-end/src/Components/Student Dashboard/DrawerFragments/HomeFragment/HomeFrag.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import DailyStudyChart from "./DailyStudyChart";
+
+export class HomeFrag extends Component {
+  render() {
+    return <DailyStudyChart />;
+  }
+}
+
+export default HomeFrag;
